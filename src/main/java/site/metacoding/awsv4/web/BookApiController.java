bba9612eea047450126b2cc481f4816acc6d7233
Yangyu <ssunyujin@naver.com>
2024-11-09 16:13:17 +0900

    bowaaaaaa

--- src/main/java/site/metacoding/awsv4/web/BookApiController.java
+++ src/main/java/site/metacoding/awsv4/web/BookApiController.java
@@ -22,7 +22,7 @@ public class BookApiController {
 
     @GetMapping("/")
     public String home() {
-        return "<h1>aws-v4</h1>";
+        return "<h1>aws-v4-1109</h1>";
     }
 
     @PostMapping("/api/book")
